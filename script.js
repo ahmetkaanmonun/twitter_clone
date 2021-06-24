cf7a6984fc31f72f9dddfb7f71875f989e690abe
Kaan Monun <ahmetkaan.monun@outlook.com>
2021-06-24 09:28:55 +0300

    sidebar added

--- script.js
+++ script.js
@@ -16,6 +16,10 @@ const postModalX = document.querySelector(".modal-header i");
 const modalPostBtn = document.querySelector(".modal-header button");
 const modalFooterPlus = document.querySelector(".modal-footer span");
 const modalInput = document.querySelector(".modal-input");
+const user = document.querySelector(".user");
+const sidebar = document.querySelector(".sidebar");
+const sidebarWrapper = document.querySelector(".sidebar-wrapper");
+const xBtn = document.querySelector(".sidebar-header i")
 /*******************************************************/
 /*******************************************************/
 
@@ -164,3 +168,20 @@ modalInput.addEventListener("blur", e => {
     }
   });
   
+
+  //Sidebar
+
+user.addEventListener("click", () => {
+    sidebar.classList.add("sidebar-display");
+    sidebarWrapper.classList.add("sidebar-wrapper-display");
+});
+
+xBtn.addEventListener("click",()=>{
+
+
+    sidebar.classList.remove("sidebar-display");
+    sidebarWrapper.classList.remove("sidebar-wrapper-display");
+
+
+
+})
