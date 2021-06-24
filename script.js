f5a1a967bea9599bd5b10ca7f1d844980690d6e2
Kaan Monun <ahmetkaan.monun@outlook.com>
2021-06-24 10:12:41 +0300

    dark mode added

--- script.js
+++ script.js
@@ -19,7 +19,9 @@ const modalInput = document.querySelector(".modal-input");
 const user = document.querySelector(".user");
 const sidebar = document.querySelector(".sidebar");
 const sidebarWrapper = document.querySelector(".sidebar-wrapper");
-const xBtn = document.querySelector(".sidebar-header i")
+const xBtn = document.querySelector(".sidebar-header i");
+const toggle = document.querySelector(".toggle");
+const circle = document.querySelector(".circle");
 /*******************************************************/
 /*******************************************************/
 
@@ -185,3 +187,20 @@ xBtn.addEventListener("click",()=>{
 
 
 })
+
+
+// dark mode
+const darkElements1 = document.querySelectorAll(".dark-mode-1");
+const darkElements2 = document.querySelectorAll(".dark-mode-2");
+const lightTexts = document.querySelectorAll(".light-text");
+const borders = document.querySelectorAll(".border");
+
+
+toggle.addEventListener("click", () => {
+  circle.classList.toggle("move");
+  Array.from(darkElements1).map(darkEl1 => darkEl1.classList.toggle("dark-1"));
+  Array.from(darkElements2).map(darkEl2 => darkEl2.classList.toggle("dark-2"));
+  Array.from(lightTexts).map(lightText => lightText.classList.toggle("light"));
+  Array.from(borders).map(border => border.classList.toggle("border-color"));
+
+});
